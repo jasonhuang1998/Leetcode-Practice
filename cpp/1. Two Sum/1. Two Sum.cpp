bd30eaaf525e280254eb62d9893bd7a7de98febd
Jason Huang <jason602920104@gmail.com>
2022-04-30 20:36:08 +0800

    Add files via upload

---
+++ cpp/1. Two Sum/1. Two Sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            for(int j = 0; j < nums.size(); j++)
+            {
+                if((nums[i] + nums[j] == target) && (i != j))
+                {
+                    ans.insert(ans.begin(), i);
+                    ans.insert(ans.begin(), j);
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+};
